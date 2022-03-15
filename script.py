443f1306ec6fafff690adc24aa6c0bb9394cca93
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-03-15 10:28:38 +0100

    version: 1.3.1 : Remplacement de la variable status des fonctions par un déclenchement d'erreur en cas de problème + ajout de message indiquant les debut et fin de fonction. Les codes erreurs ont également été repris

--- script.py
+++ script.py
@@ -24,9 +24,10 @@ domaine = 'http://books.toscrape.com'
 
 
 def extract_info_livre(url):
+    print("[DEBUT]extract_info_livre:", url)
     page = requests.get(url)
     livre = {}
-    status = "000:extraction livre ok"
+
     if page.ok:
         livre['product_page_url'] = url
         try:
@@ -40,16 +41,20 @@ def extract_info_livre(url):
             livre['description'] = soup.find(id='product_description').find_next_sibling('p').string
             livre['review_rating'] = soup.find('p', class_='star-rating')['class'][1]
             livre['image_url'] = soup.find('div', class_='thumbnail').find('img')['src'].replace('../..', domaine)
-        except AttributeError:
+        except AttributeError as exc:
             livre = {}
-            status = "001:problème d'attribut lors de l'extraction d'un livre"
+            raise ValueError("001:[extraction informations d'un livre]cette erreur se produit soit parce que l'url "
+                             "de la page à extraire est erronée et/ou ne concerne pas une page livre")
     else:
-        status = "002:page inaccessible lors de l'extraction"
+        raise ValueError("002:[extraction informations d'un livre]cette erreur se produit au moment de l'extraction "
+                         "d'une page de type livre; la page demandée n'est pas accessible")
 
-    return status, livre
+    print("[FIN]extract_info_livre:", url, "infos:", livre)
+    return livre
 
 
 def extraire_urls_livres_par_categorie(url):
+    print("[DEBUT]extract_urls_livre_par_catégorie:", url)
     def reforme_url(url, num_page):
         url_split = url.split('/')
         url_split = url_split[0:7]
@@ -57,11 +62,13 @@ def extraire_urls_livres_par_categorie(url):
         url = "/".join(url_split)
         return url
 
-    status = "000:extraction livres ok"
     num_page = 1
     url_reforme = reforme_url(url, num_page)
     urls_livres = []
     page = requests.get(url_reforme)
+    if not page.ok:
+        raise ValueError("004:[extraction urls des livres d'une page categorie]cette erreur se produit au moment"
+                         " de l'extraction d'une page de type livre; la page demandée n'est pas accessible")
     while page.ok:
         try:
             soup = BeautifulSoup(page.content, 'html.parser')
@@ -69,74 +76,82 @@ def extraire_urls_livres_par_categorie(url):
 
             for link in links:
                 urls_livres.append(link.find("a")['href'].replace("../../..", domaine + "/catalogue"))
-
+            print("[EN COURS]extract_urls_livre_par_catégorie page", num_page, "nombre de livres total:",
+                  str(len(urls_livres)))
             num_page += 1
             url_reforme = reforme_url(url, num_page)
             page = requests.get(url_reforme)
+            print("[FIN]extract_urls_livre_par_catégorie:", url_reforme)
 
         except AttributeError:
-            status = "001:problème d'attribut lors de l'extraction de la liste de livre"
+            raise ValueError("003:[extraction urls des livres d'une page categorie]cette erreur se produit "
+                             "soit la page a évoluée, soit l'url de la page à extraire est erronée "
+                             "et/ou ne concerne pas une page catégorie")
             urls_livres = []
-    return status, urls_livres
+    return urls_livres
 
 
 def extract_info_livres_par_categorie(url_categorie):
-    status, url_livres = extraire_urls_livres_par_categorie(url_categorie)
+    url_livres = extraire_urls_livres_par_categorie(url_categorie)
     livres = []
-    if status[0:3] == "000":
-        for url in url_livres:
-            livres.append(extract_info_livre(url)[1])
-    return status, livres
+    for url in url_livres:
+        livres.append(extract_info_livre(url))
+    return livres
 
 
 def export_csv(livres):
-    status = "000:export csv ok"
-    if not (os.path.exists("data")):
-        os.mkdir("data")
-    nom_du_fichier = "data/" + livres[0]['category'] + ".csv"
-    en_tete = []
-    en_tete = list(livres[0].keys())
+    if len(livres) > 0:
 
-    with open(nom_du_fichier, "w") as fichier_csv:
-        writer = csv.writer(fichier_csv, delimiter=",")
-        writer.writerow(en_tete)
-        for livre in livres:
-            ligne = list(livre.values())
-            writer.writerow(ligne)
+        print("[DEBUT]export: de ", len(livres), "livre(s) dont le premier est :", livres[0])
+        if not (os.path.exists("data")):
+            os.mkdir("data")
+        nom_du_fichier = "data/" + livres[0]['category'] + ".csv"
 
-    return status
+        en_tete = list(livres[0].keys())
+
+        with open(nom_du_fichier, "w") as fichier_csv:
+            writer = csv.writer(fichier_csv, delimiter=",")
+            writer.writerow(en_tete)
+            for livre in livres:
+                ligne = list(livre.values())
+                writer.writerow(ligne)
+        print("[FIN]export: de ", len(livres), "livre(s) dont le dernier est :", livres[len(livres)-1])
+    else:
+        print("[WARNING] Aucun livre à exporter")
 
 
 livres = []
 '''
 # url = "http://books.toscrape.com/catalogue/soumission_998/index.html"
 url = "http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
-status, livres = extract_info_livres_par_categorie(url)
+livres = extract_info_livres_par_categorie(url)
 export_csv(livres)
 print(len(livres))
 '''
+try:
+    print("Saisir 1 pour extraire les informations d'un livre et 2 pour les informations d'une catégorie, laisser vide"
+          "pour quitter")
+    choix = input()
+    if choix in ["1", "2"]:
+        url = -1
+        while url:
+            print(
+                "Entrer l'url concernant les infos à extraire du site http://books.toscrape.com/ ou laisser vide pour quitter:")
+            url = input()
+            if url != "":
+                if choix == "1":
+                    livre = extract_info_livre(url)
+                    if livre:
+                        print(livre)
 
-print("Saisir 1 pour extraire les informations d'un livre et 2 pour les informations d'une catégorie, laisser vide"
-      "pour quitter")
-choix = input()
-if choix in ["1", "2"]:
-    url = -1
-    while url:
-        print(
-            "Entrer l'url concernant les infos à extraire du site http://books.toscrape.com/ ou laisser vide pour quitter:")
-        url = input()
-        if url != "":
-            if choix == "1":
-                status, livre = extract_info_livre(url)
-                if livre:
-                    print(livre)
-                    print(status)
-                    livres.append(livre)
+                        livres.append(livre)
 
-                if len(livres) > 0:
-                    resultat = export_csv(livres)
-                    print(resultat)
-            elif choix == "2":
-                status, livres = extract_info_livres_par_categorie(url)
-                resultat = export_csv(livres)
-                print(resultat)
+                    if len(livres) > 0:
+                        export_csv(livres)
+
+                elif choix == "2":
+                    livres = extract_info_livres_par_categorie(url)
+                    export_csv(livres)
+
+except ValueError as exc:
+    print(exc)
