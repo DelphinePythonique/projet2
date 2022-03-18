aed8c89bea13b2d6f32c1fcc62ff1d9563a82a01
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-03-18 11:17:20 +0100

    - Les fonctions utiles au script ont été déplacées dans un fichier functions.py -mise en place d'une gestion des logs -mise en place de test de type doctest

---
+++ functions.py
@@ -0,0 +1,272 @@
+import os
+import shutil
+from doctest import testmod
+import logging
+import csv
+from itertools import groupby
+
+import requests
+from bs4 import BeautifulSoup
+
+DOMAINE = 'http://books.toscrape.com'
+
+logging.basicConfig(filename='std.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def extract_info_livre(url_du_livre_a_extraire):
+
+    """
+    Extraction des informations d'un livre
+
+        :param url_du_livre_a_extraire : Url de la page concernant un livre à extraire
+        :type url_du_livre_a_extraire: str
+        :return: livre_a_extraire: Dictionnaire contenant l'ensemble des informations du livre
+        :rtype:  dict
+
+    >>> extract_info_livre("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")  # doctest: +NORMALIZE_WHITESPACE
+    {'product_page_url': 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html',
+    'upc': 'a897fe39b1053632',
+    'title': 'A Light in the Attic',
+    'price_including_tax': '£51.77',
+    'price_excluding_tax': '£51.77',
+    'number_available': 'In stock (22 available)',
+    'category': 'Poetry',
+    'description': "It's hard to imagine a world without A Light in the Attic.
+    This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th
+    anniversary with this special edition. Silverstein's humorous and creative verse can amuse the
+    dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic
+    words and laugh and smile and love th It's hard to imagine a world without A Light in the Attic.
+    This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th
+    anniversary with this special edition. Silverstein's humorous and creative verse can amuse the
+    dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic words
+    and laugh and smile and love that Silverstein. Need proof of his genius? RockabyeRockabye baby,
+    in the treetopDon't you know a treetopIs no safe place to rock?And who put you up there,And
+    your cradle, too?Baby, I think someone down here'sGot it in for you. Shel, you never sounded so
+    good. ...more",
+    'review_rating': 'Three',
+    'image_url': 'http://books.toscrape.com/media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg'}
+
+    """
+
+    logger.info(f"[DEBUT]extract_info_livre:{url_du_livre_a_extraire}")
+    page = requests.get(url_du_livre_a_extraire)
+    livre_a_extraire = {}
+
+    if page.ok:
+        livre_a_extraire['product_page_url'] = url_du_livre_a_extraire
+        try:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            livre_a_extraire['upc'] = soup.find('th', string='UPC').find_next_sibling('td').string
+            livre_a_extraire['title'] = soup.find('h1').string
+            livre_a_extraire['price_including_tax'] = soup.find('th', string='Price (incl. tax)') \
+                .find_next_sibling('td').string
+            livre_a_extraire['price_excluding_tax'] = soup.find('th', string='Price (excl. tax)') \
+                .find_next_sibling('td').string
+            livre_a_extraire['number_available'] = soup.find('th', string='Availability') \
+                .find_next_sibling('td').string
+            livre_a_extraire['category'] = soup.find('ul', class_="breadcrumb") \
+                .findAll("li")[-2].find("a").string
+            if soup.find(id='product_description'):
+                livre_a_extraire['description'] = soup.find(id='product_description') \
+                    .find_next_sibling('p').string
+            else:
+                livre_a_extraire['description'] = ""
+
+            livre_a_extraire['review_rating'] = soup.find('p', class_='star-rating')['class'][1]
+            livre_a_extraire['image_url'] = soup.find('div', class_='thumbnail').find('img')['src'] \
+                .replace('../..', DOMAINE)
+        except AttributeError as erreur_extraction:
+            raise ValueError("001:[extraction informations d'un livre]cette erreur se produit \n "
+                             "soit parce que l'url de la page à extraire est erronée\n "
+                             "et/ou ne concerne pas une page livre.\n "
+                             "Detail de l'erreur:" + str(erreur_extraction)) from erreur_extraction
+    else:
+        raise ValueError(
+            "002:[extraction informations d'un livre]cette erreur se produit au moment de "
+            "l'extraction d'une page de type livre; la page demandée n'est pas accessible")
+
+    logger.info(f"[FIN]extract_info_livre:{url_du_livre_a_extraire}, infos:{livre_a_extraire}")
+    return livre_a_extraire
+
+
+def extraire_urls_livres_par_categorie(url_categorie_des_livres_a_extraire):
+    """
+     Extraction des urls des livres d'une catégorie
+
+    Parameters
+    url_categorie_des_livres_a_extraire (str)
+
+    Returns
+    urls_livres list
+    """
+
+    def reforme_url(url, num_page_active):
+        url_split = url.split('/')
+        url_split = url_split[0:7]
+        url_split.append('page-' + str(num_page_active) + ".html")
+        url = "/".join(url_split)
+        return url
+
+    num_page = 1
+
+    urls_livres = []
+    page = requests.get(url_categorie_des_livres_a_extraire)
+    logger.info(f"[DEBUT]extract_urls_livre_par_catégorie:{url_categorie_des_livres_a_extraire}")
+    if not page.ok:
+        raise ValueError(
+            f"004:[extraction urls des livres d'une page categorie]cette erreur se produit au moment"
+            " de l'extraction d'une page de type livre; la page demandée n'est pas accessible")
+    while page.ok:
+        try:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            links = soup.find_all("h3")
+
+            for link in links:
+                urls_livres.append(
+                    link.find("a")['href'].replace("../../..", DOMAINE + "/catalogue"))
+            logger.info(f"[EN COURS]extract_urls_livre_par_catégorie page {num_page} "
+                        f"nombre de livres total:{str(len(urls_livres))}")
+            num_page += 1
+            url_reforme = reforme_url(url_categorie_des_livres_a_extraire, num_page)
+            page = requests.get(url_reforme)
+            logger.info(f"[FIN]extract_urls_livre_par_catégorie:{url_reforme}")
+
+        except AttributeError as erreur_extraction:
+            raise ValueError(
+                f"003:[extraction urls des livres d'une page categorie]cette erreur se produit "
+                f"soit la page a évoluée, soit l'url de la page à extraire est erronée "
+                f"et/ou ne concerne pas une page catégorie") from erreur_extraction
+
+    return urls_livres
+
+
+def extract_info_livres_par_categorie(url_categorie_livres_a_extraire):
+    """
+    Extrait les informations concernant des livres correspondant aux urls transmises
+    :param url_categorie_livres_a_extraire: list
+    :return: livres_a_extraire: list
+    """
+    url_livres = extraire_urls_livres_par_categorie(url_categorie_livres_a_extraire)
+    livres_a_extraire = []
+    for url in url_livres:
+        livres_a_extraire.append(extract_info_livre(url))
+    return livres_a_extraire
+
+
+def extract_urls_categorie(url_origine):
+    """
+    Extrait les urls des catégories de l'url du site transmis
+    :param url_origine: str
+    :return: urls_categorie : list
+    """
+    page = requests.get(url_origine)
+    if page.ok:
+        logger.info(f"[DEBUT]export urls des categories à partir de l'url {url_origine}")
+        soup = BeautifulSoup(page.content, "html.parser")
+        links = soup.find('div', class_="side_categories").find_all('a')
+        links.pop(0)  # suppression de la catégorie parent Books
+        urls_categorie = []
+        for link in links:
+            urls_categorie.append(url_origine + "/" + link['href'])
+        logger.info(f"[FIN]export urls des catégories à partir de l'url {url_origine} "
+                    f"de {len(urls_categorie)} catégories")
+
+    else:
+        raise ValueError("005:[extraction urls des categories]cette erreur se produit "
+                         "car la page demandée n'est pas accessible")
+    return urls_categorie
+
+
+def extract_all(url_origine):
+    """
+    Génère les fichiers csv de l'ensemble des livres; avec un fichier csv par catégorie de livre
+    :param url_origine: str
+    :return: livres_extraits list
+    """
+    logger.info(f"[DEBUT]extraction intégral de: {url_origine}")
+    urls_categorie = extract_urls_categorie(url_origine)
+    livres_extraits = []
+    for url in urls_categorie:
+        livres_extraits.extend(extract_info_livres_par_categorie(url))
+    logger.info(f"[FIN]extraction intégral de:{url_origine}")
+    return livres_extraits
+
+
+def export_csv(elements, regroupement_par):
+    """
+    Enregistre les elements dans un fichier csv
+    :param regroupement_par: str
+    :param elements: list
+    :return: None
+    """
+
+    def key_func(k):
+        return k[regroupement_par]
+
+    if len(elements) > 0:
+        elements = sorted(elements, key=key_func)
+
+        logger.info(f"[DEBUT]export: de {len(elements)} ligne(s) "
+                    f"dont le premier est : {elements[0]}")
+        if not os.path.exists("data"):
+            os.mkdir("data")
+
+        for key, value in groupby(elements, key_func):
+
+            nom_du_fichier = "data/" + key + ".csv"
+
+            elements_regroupes = list(value)
+            en_tete = list(elements_regroupes[0].keys())
+
+            with open(nom_du_fichier, "w", encoding="utf-8") as fichier_csv:
+                writer = csv.writer(fichier_csv, delimiter=",")
+                writer.writerow(en_tete)
+                for element_regroupe in elements_regroupes:
+                    ligne = list(element_regroupe.values())
+                    writer.writerow(ligne)
+        logger.info(f"[FIN]export")
+    else:
+        logger.info(f"[WARNING] Aucun livre à exporter")
+
+
+def telecharger_image(url_de_l_image, path, nom_fichier):
+    """
+    Télécharge l'image correspondant à l'url_image
+    :param url_de_l_image: str
+    :param path: str
+    :param nom_fichier: str
+    :return: none
+    """
+    print("[DEBUT]Télécharger image  ", "chemin", path, "nom fichier ", nom_fichier)
+    path_exist = os.path.exists(path)
+    if not path_exist:
+        os.makedirs(path)
+    page = requests.get(url_de_l_image, stream=True)
+    if page.ok:
+        page.raw.decode_content = True
+
+        with open(path + nom_fichier, 'wb') as fichier_image:
+            shutil.copyfileobj(page.raw, fichier_image)
+    logger.info("[FIN]Télécharger image  ", url_de_l_image, "chemin", path, "nom fichier ",
+                nom_fichier)
+
+
+def telecharger_images():
+    """
+    Télécharge les images dont l'url_image est reprise dans les fichiers csv générés par
+    l'application et les mémorise dans le répertoire data/images
+    :return:
+    """
+    path = "data"
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith('.csv'):
+            with open(path + "/" + file, encoding="utf-8") as fichier_csv:
+                dict_reader = csv.DictReader(fichier_csv, delimiter=",")
+                for row in dict_reader:
+                    url = row['image_url']
+                    nom_fichier_a_telecharger = row['upc'] + ".jpg"
+                    telecharger_image(url, "data/images/", nom_fichier_a_telecharger)
